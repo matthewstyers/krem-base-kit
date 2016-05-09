e6d1d30bbd2faf7c1f2d99332574b9704723ebd7
Matthew Styers <mattysty@gmail.com>
2016-05-08 22:51:09 -0500

    restores default script source on server

--- server/routes/views/index.js
+++ server/routes/views/index.js
@@ -6,5 +6,7 @@ exports = module.exports = function(req, res) {
   res.locals.scriptSrc = undefined;
   view.render('index');
 
-
+  res.locals.scriptSrc = process.env.NODE_ENV === 'development' ?
+    `http://${process.env.DOCKER_MACHINE_IP}:8080/bundle.js` :
+    undefined;
 };
